b607e4aa9a28646c37694c38d66cf677fd31aebb
kaku2792 <67434630+kaku2792@users.noreply.github.com>
2020-12-28 21:41:56 -0700

    part three commit 3 change where income table

--- schema_part_one.sql
+++ schema_part_one.sql
@@ -52,8 +52,10 @@ FROM "Salaries_by_Region" as sr
 INNER JOIN "Salaries_by_College_Type" as sc
 ON sr."School_Name" = sc."School_Name";
 
-CREATE TABLE "Income_Outcome"
 
 --DROP TABLE School_Region_Salaries;
 
 SELECT * From School_Region_Salaries
+
+
+CREATE TABLE "Income_Outcome"
